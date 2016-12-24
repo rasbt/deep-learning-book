c5c2eb40513d6c1bdb362ff6704ee819ef352570
rasbt <mail@sebastianraschka.com>
2016-12-24 12:08:04 -0500

    ann pkg template

---
+++ ann/__init__.py
@@ -0,0 +1,11 @@
+# Sebastian Raschka 2016-2017
+#
+# Supporting package for the book "
+# 'Introduction to Artificial Neural Networks and Deep Learning:
+#    A Practical Guide with Applications in Python'
+#
+# Author: Sebastian Raschka <sebastianraschka.com>
+#
+# License: MIT
+
+__version__ = '0.1.0'
