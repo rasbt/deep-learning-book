2580b92956b88930f283100c84941b00024ced43
rasbt <mail@sebastianraschka.com>
2016-12-25 01:35:59 -0500

    .travis.yml

--- .travis/install_travis_env.sh
+++ .travis/install_travis_env.sh
@@ -17,9 +17,9 @@ export PATH="$HOME/miniconda/bin:$PATH"
 # Setting up the Test Environment
 
 if [ "${LATEST}" = "true" ]; then
-  create -q -n testenv --yes python=$TRAVIS_PYTHON_VERSION numpy
+  conda create -q -n testenv --yes python=$TRAVIS_PYTHON_VERSION numpy
 else
-  create -q -n testenv --yes python=$TRAVIS_PYTHON_VERSION numpy=$NUMPY_VERSION
+  conda create -q -n testenv --yes python=$TRAVIS_PYTHON_VERSION numpy=$NUMPY_VERSION
 fi
 
 source activate testenv
