1c9714ab20bd1761c3de3a6c97379002ee5b6b59
rasbt <mail@sebastianraschka.com>
2016-12-29 23:54:31 -0500

    put an error into that notebook for testing

--- .travis/run_travis_tests.sh
+++ .travis/run_travis_tests.sh
@@ -5,5 +5,5 @@ set -e
 nosetests ann
 
 if [[ "$NOTEBOOKS" == "true" ]]; then
-    find code -name "*.ipynb" -exec jupyter nbconvert --to notebook --execute {} \;
+    find . -name "*.ipynb" -exec jupyter nbconvert --to notebook --execute {} \;
 fi
