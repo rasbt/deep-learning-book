c56ad7ba11430cebb452d6091d5413e8fc34a2df
rasbt <mail@sebastianraschka.com>
2016-12-30 03:49:11 -0500

    put an error into that notebook for testing

--- code/test_notebooks.py
+++ code/test_notebooks.py
@@ -11,24 +11,11 @@ def run_ipynb(path):
         args = ["python", "-m", "nbconvert", "--to",
                 "notebook", "--execute", "--output",
                 fout.name, path]
-        print(" ".join(args))
-        subprocess.check_call(args)
-        fout.seek(0)
-
-        # 'byte' vs str issues when running the following in Py 3.5
-        nb = nbformat.read(fout, nbformat.current_nbformat)
-
-    for cell in nb.cells:
-        for output in cell["outputs"]:
-            if output.output_type == "error":
-                error_cells.append(output)
-
-    return nb, errors
+        subprocess.check_output(args)
 
 
 def test_appendix_g_tensorflow_basics():
     this_dir = os.path.dirname(os.path.abspath(__file__))
 
-    ipynb, errors = run_ipynb(os.path.join(this_dir,
-                              'appendix_g_tensorflow-basics.ipynb'))
-    assert errors == []
+    run_ipynb(os.path.join(this_dir,
+                           'appendix_g_tensorflow-basics.ipynb'))
