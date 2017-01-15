cb7cd445eba6cef0d8544ef497045bd03c887031
rasbt <mail@sebastianraschka.com>
2017-01-15 03:02:01 -0500

    update header lines

--- ann/__init__.py
+++ ann/__init__.py
@@ -1,8 +1,8 @@
 # Sebastian Raschka 2016-2017
 #
-# Supporting package for the book "
-# 'Introduction to Artificial Neural Networks and Deep Learning:
-#    A Practical Guide with Applications in Python'
+# ann is a supporting package for the book
+# "Introduction to Artificial Neural Networks and Deep Learning:
+#  A Practical Guide with Applications in Python"
 #
 # Author: Sebastian Raschka <sebastianraschka.com>
 #
