8129a69f3118d5d959137935c5257e9c1607f6d8
rasbt <mail@sebastianraschka.com>
2017-02-25 00:54:45 -0500

    section on tensorflow model reuse

--- code/tests/test_notebooks.py
+++ code/tests/test_notebooks.py
@@ -20,7 +20,7 @@ class TestNotebooks(unittest.TestCase):
     def test_appendix_g_tensorflow_basics(self):
         this_dir = os.path.dirname(os.path.abspath(__file__))
         os.chdir(this_dir)
-        run_ipynb('./appendix_g_tensorflow-basics.ipynb')
+        run_ipynb('../appendix_g_tensorflow-basics.ipynb')
 
 
 if __name__ == '__main__':
