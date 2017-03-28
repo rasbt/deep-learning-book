435f28bb2b98f545fffdfd0e254e4d53dc4a3391
rasbt <mail@sebastianraschka.com>
2017-03-28 18:55:08 -0400

    pep8 fixes

--- code/ch02_perceptron/solutions/01_weight_zero_learning_rate.py
+++ code/ch02_perceptron/solutions/01_weight_zero_learning_rate.py
@@ -5,11 +5,13 @@ for _ in range(2):
     _ = perceptron_train(X_train, y_train, mparams=small_lr_params)
 
 x_min_small = -2
-y_min_small = (-(small_lr_params['weights'][0] * x_min) / small_lr_params['weights'][1] -
+y_min_small = (-(small_lr_params['weights'][0] * x_min) /
+               small_lr_params['weights'][1] -
                (small_lr_params['bias'] / small_lr_params['weights'][1]))
 
 x_max_small = 2
-y_max_small = (-(small_lr_params['weights'][0] * x_max) / small_lr_params['weights'][1] -
+y_max_small = (-(small_lr_params['weights'][0] * x_max) /
+               small_lr_params['weights'][1] -
                (small_lr_params['bias'] / small_lr_params['weights'][1]))
 
 
@@ -18,11 +20,15 @@ fig, ax = plt.subplots(1, 2, sharex=True, figsize=(7, 3))
 ax[0].plot([x_min, x_max], [y_min, y_max])
 ax[1].plot([x_min_small, x_max_small], [y_min_small, y_max_small])
 
-ax[0].scatter(X_train[y_train==0, 0], X_train[y_train==0, 1], label='class 0', marker='o')
-ax[0].scatter(X_train[y_train==1, 0], X_train[y_train==1, 1], label='class 1', marker='s')
+ax[0].scatter(X_train[y_train == 0, 0], X_train[y_train == 0, 1],
+              label='class 0', marker='o')
+ax[0].scatter(X_train[y_trai == 1, 0], X_train[y_train == 1, 1],
+              label='class 1', marker='s')
 
-ax[1].scatter(X_train[y_train==0, 0], X_train[y_train==0, 1], label='class 0', marker='o')
-ax[1].scatter(X_train[y_train==1, 0], X_train[y_train==1, 1], label='class 1', marker='s')
+ax[1].scatter(X_train[y_train == 0, 0], X_train[y_train == 0, 1],
+              label='class 0', marker='o')
+ax[1].scatter(X_train[y_train == 1, 0], X_train[y_train == 1, 1],
+              label='class 1', marker='s')
 
 ax[1].legend(loc='lower right')
 
